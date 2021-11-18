b52ba44b91b04f3e62700e04c5fd42426b265f76
c-blaq <babayejuadesina@gmail.com>
2021-11-18 18:11:33 +0100

    working on search route

--- routes/search.js
+++ routes/search.js
@@ -3,11 +3,16 @@ const { Hostel } = require("../model/hostel");
 const express = require("express");
 const router = express.Router();
 
+router.post("/hostels", async (req, res) => {
+  let hostel = new Hostel(req.body);
+  hostel = await hostel.save();
+});
+
 router.get("/hostels/", async (req, res) => {
   let { q: search } = req.query;
   search = (search || "").trim();
 
-  let hostels = await Hostel.find().sort({ name: 1 });
+  let hostels = await Hostel.find().sort({ hostelName: 1 });
   if (search) {
     let hostel = hostels
       .filter(
@@ -15,15 +20,19 @@ router.get("/hostels/", async (req, res) => {
           hostel.location.toLowerCase().indexOf(search.toLowerCase()) !== -1
       )
       .map((hostel) => hostel);
+
+    // console.log(hostel);
+
+    res.render("search-result", { hostel });
+
     //   res.render("explore", { hostel });
     // } else {
     //   res.redirect("/");
   }
 });
 
-router.post("/hostels", async (req, res) => {
-  let hostel = new Hostel(req.body);
-  hostel = await hostel.save();
+router.get("/search", (_, res) => {
+  res.render("search-result");
 });
 
 module.exports = router;
